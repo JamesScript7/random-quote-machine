4a958bd24f21c4668cddcdd15facffde957508a6
James Kim <james.kim7@yahoo.com>
2017-04-25 23:54:59 -0400

    Added a conditional to fix repeating quotes
    Quotes will be different than the previous quote
    - Math.random() will sometimes pick the same number

--- scripts.js
+++ scripts.js
@@ -1,19 +1,31 @@
 $(document).ready(function() {
-  var quoteField = document.getElementById('quote-field');
-  var quoteButton = document.getElementById('quote-btn');
-  var quotes = {
+  var
+    quoteField = document.getElementById('quote-field'),
+    quoteButton = document.getElementById('quote-btn'),
+    quotes = {
     "Maya": "My mission in life is not merely to survive...",
     "Dita": "Some days are just bad days, that\'s all. You have to experience...",
-    "Allen": "Your attitude is like a box of crayons that color your world..."
+    "Allen": "Your attitude is like a box of crayons that color your world...",
+    "James": "You can only YOLO once...",
+    "Jewel": "I love furries!"
   };
+
   function pickRandomQuote(obj) {
-    var keys = Object.keys(obj)
+    // This will put the keys in an array so we can use the index for random assignment.
+    var keys = Object.keys(obj);
+
     return obj[keys[keys.length * Math.random() << 0]];
   }
 
-  quoteButton.addEventListener('click', function() {
-    quoteField.innerHTML = pickRandomQuote(quotes);
-  });
+  var msg = pickRandomQuote(quotes);
 
+  quoteButton.addEventListener('click', function() {
+    quoteField.innerHTML = msg;
+
+    // This is here because the Math.random() function returned the same keys at times.
+    while (quoteField.innerHTML === msg) {
+      msg = pickRandomQuote(quotes);
+    }
+  });
 
 });
