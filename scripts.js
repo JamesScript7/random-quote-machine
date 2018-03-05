1aca1190e1116536bf2ba6d63abdd95a050ebd38
James Kim <yameskim@gmail.com>
2018-03-04 23:59:42 -0500

    image loads apporiate size for mobile

--- scripts.js
+++ scripts.js
@@ -1,24 +1,42 @@
 $(document).ready(function() {
   $('#quote-box').hide();
 
+  // Image related
+  var backgroundImg,
+      movePixel = 60;
+  // if mobile { load 1024x768 image else 2048x1536 }
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    movePixel = 200;
+    backgroundImg = 'https://picsum.photos/1024/768/?image=';
+  } else {
+    backgroundImg = 'https://picsum.photos/2048/1536/?image=';
+  }
+
   var
-    // Image related
-    backgroundImg = 'https://picsum.photos/1024/768/?image=',
     imageNumber = randomImgNum(),
     // Quote related
+    quoteJSON = {},
     quoteField = document.getElementById('quote-field'),
     quoteButton = document.getElementById('quote-btn'),
     tweetBtn = document.getElementById('btn'),
-    urlQuotes = 'https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520';
+    urlQuotes = 'https://gist.githubusercontent.com/dmakk767/9375ff01aff76f1788aead1df9a66338/raw/491f8c2e91b7d3b8f1c8230e32d9c9bc1a1adfa6/Quotes.json%2520',
+    // Quote Parsing
+    rand,
+    authorHashTag,
+    fullQuote;
 
+  // Gets JSON once and stores it in quoteJSON
+  getJSON();
+
+  // Random Number Generators
   function randomQuoteNum(x) {
     return (x.length * Math.random() << 0);
   };
-
   function randomImgNum() {
     return Math.round(Math.random() * 1084);
   }
 
+  // Tweet formatter
   function tweetFormat(quote, author) {
     var res = quote;
     var lenWithoutAuthor = 119 - author.length;
@@ -31,69 +49,75 @@ $(document).ready(function() {
     return res;
   }
 
-  function getJson(handleData) {
+  // Quote JSON function
+  function getJSON() {
     $.ajax({
       url: urlQuotes,
+      error: function() {
+        setTimeout(function(){
+          getJSON();
+        }, 100);
+      },
       success: function(data) {
-        handleData(data);
+        quoteJSON = JSON.parse(data);
       },
       cache: 'false'
     });
   };
 
   // Main function that retrieves and loads the content.
-  function loadIMG() {
+  function loadContent() {
     imageNumber = randomImgNum();
+    rand = randomQuoteNum(quoteJSON);
 
-    $('#image').fadeOut(500, function() {
+    $('#image').fadeOut(1000, function() {
       $.ajax({
         url: (backgroundImg + imageNumber),
         error: function(err) {
           // console.log(err.status);
-          loadIMG();
+          setTimeout(function() {
+            loadContent();
+          }, 100);
         },
         success: function() {
-          // console.log('success');
+          // Prep Quote string
+          authorHashTag = quoteJSON[rand].name.replace(/\s/g, '');
+          fullQuote = tweetFormat(quoteJSON[rand].quote, authorHashTag);
+
+          // Set image
           $('#image').attr('src', backgroundImg + imageNumber);
 
           document.getElementById('image').onload = function() {
 
+            // Image resetter
+            $('.image-container').stop();
+            $('.image-container').css('left', 0);
+
             // Image moving effect
             $('.image-container').animate({
-              left: '-=30',
+              left: ('-=' + movePixel),
               easing: 'easeInOutSine'
-            }, 8000);
+            }, 9000);
 
             $('#quote-box').show();
             $('#image').fadeIn(1000);
 
-            // Gets and parses quote
-            getJson(function(out) {
-              var
-                quoteList = JSON.parse(out),
-                rand = randomQuoteNum(quoteList),
-                authorHashTag = quoteList[rand].name.replace(/\s/g, ''),
-                fullQuote = tweetFormat(quoteList[rand].quote, authorHashTag);
+            // Quote box content
+            quoteField.innerHTML = `${quoteJSON[rand].quote} - ${quoteJSON[rand].name}`;
+            tweetBtn.innerHTML = '';
 
-              quoteField.innerHTML = `${quoteList[rand].quote} - ${quoteList[rand].name}`;
-              tweetBtn.innerHTML = '';
+            $('#quote-field').hide();
+            $('#quote-field').fadeIn(1100);
 
-              $('#quote-field').hide();
-              $('#quote-field').fadeIn(1100, function() {
-                return quoteField;
-              });
-
-              twttr.widgets.createShareButton(
-                '/',
-                document.getElementById('btn'),
-                {
-                  text: fullQuote,
-                  hashtags: authorHashTag,
-                  via: 'itJamesKim'
-                }
-              );
-
-            });
+            twttr.widgets.createShareButton(
+              '/',
+              document.getElementById('btn'),
+              {
+                text: fullQuote,
+                hashtags: authorHashTag,
+                via: 'itJamesKim'
+              }
+            );
 
           }
         }
@@ -102,11 +126,11 @@ $(document).ready(function() {
   }
 
   // Initializes image and quote
-  loadIMG();
+  loadContent();
 
   // Image and quote refreshes on button click
   $('#quote-btn').click(function() {
-    loadIMG();
+    loadContent();
   });
 
   // function pickRandomQuote(obj) {
